61f94274d1addda6afb5400cbe2f581de51ecd2e
Aaron Planell Lopez <aplanell@meteo.cat>
2017-03-30 17:49:12 +0200

    Repair following this instructions: https://github.com/facebook/react/issues/9244

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import App from './App';
 
+jest.mock('rc-tabs/lib/ScrollableInkTabBar.js', () => {
+  return (props) => <div>{props.children}</div>;
+});
+
+jest.mock('react-dom', () => ({
+  findDOMNode() { return null; },
+  render() { return null; }
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
